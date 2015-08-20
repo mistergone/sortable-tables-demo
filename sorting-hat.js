5c3627263bf057e50b48e0517bf074f2b1ff2a2c
Chuck Werner <mistergone@gmail.com>
2015-08-20 10:08:15 -0400

    A few fixes for the demo.

--- sorting-hat.js
+++ sorting-hat.js
@@ -7,37 +7,44 @@ $(document).ready( function() {
         $cell = $( this ).closest( '.sortable-table_header' ),
         $row = $( this ).closest( 'tr' );
         index = $row.children( 'th, td' ).index( $cell ),
-        rows = [],
-        sorterArray = [];
-
-      console.log( $cell, index );
+        rows = [], // Array-of-Arrays for sorting
+        sign = 1; // for reverse-sorting
 
       $table.find( 'tbody tr' ).each( function() {
-        var key = $(this).find( 'td:nth-child(' + index + ')' ).text().trim();
-        console.log( index, key );
+        // Find the value of the cell we're sorting by, add it to the Array
+        var child = index + 1,
+            key = $(this).find( 'td:nth-child(' + child + ')' ).text().trim();
         rows.push( [key, $(this)] );
       });
 
+      // For reverse sorting, reverse the sign
+      if ( $(this).hasClass( 'sorter_down' ) === true ) {
+        sign = -1;
+      }
+      
       rows.sort( function( a, b ) {
+        // Set a and b to the first Array in each Array-of-Arrays
         a = a[0];
         b = b[0];
 
+        // Sort the values
         if ( a < b ) {
-          return -1
+          return sign * -1;
         }
         else {
           if ( a > b ) {
-            return 1;
+            return sign;
           }
           else {
             return 0;
           }
         }
       });
-      if ( $(this).hasClass( 'sorter_down' ) === true ) {
-        rows.reverse();
-      }
+
+      // Empty the tbody to prepare for sorted rows
       $tbody.empty();
+
+      // Insert sorted rows
       for( var i = 0; i < rows.length; i++ ) {
         $tbody.append( rows[i][1] );
       }
